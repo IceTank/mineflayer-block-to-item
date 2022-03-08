84e0e67a97645bd8a07769c7df36e5cd8eb5383c
Niklas <niklaslehle@gmail.com>
2022-03-08 19:50:29 +0100

    Code refactor Add readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# Try placing all items and record the resulting block
+
+### Install
+`npm i`
+
+### Run
+`node index.js`
+Will result in node-minecraft-warp downloading all necessary server, launching the server and connecting the bot for every supported version of Mineflayer.supportedVersions.
+Produces a list off item to block names under data/.
--- index.js
+++ index.js
@@ -2,130 +2,121 @@ const wrap = require('minecraft-wrap')
 const mf = require('mineflayer')
 const path = require('path')
 const fs = require('fs')
+const wait = require('util').promisify(setTimeout)
+const { Vec3 } = require('vec3')
+
+const { launchServer } = require('./lib/wrappedServer')
+const { launchBot } = require('./lib/wrappedBot')
+const PItem = require('prismarine-item')
+
+function onceWithTimeout(emitter, event, timeout = 5000) {
+  return new Promise((resolve, reject) => {
+    const cleanup = () => {
+      emitter.removeListener(event, listener)
+      clearTimeout(timeoutHandle)
+    }
+
+    const listener = (...args) => {
+      cleanup()
+      resolve(args)
+    }
+    const timeoutHandle = setTimeout(() => {
+      cleanup()
+      reject(new Error('timeout'))
+    }, timeout)
+
+    emitter.once(event, listener)
+  })
+}
 
 const Versions = ['1.16']
 
 const jarsPath = path.join('./jars')
 const serverPath = path.join('./servers')
+const dataOutput = path.join('./data')
 
 const serverOptions = require('./serverOptions.json')
-const { once } = require('events')
-
-const version = '1.16.5'
+// const { once } = require('events')
 
 async function runBlocks(version) {
   const jarFile = path.resolve(path.join(jarsPath, version + '.jar'))
   const serverDir = path.resolve(path.join(serverPath, version))
+  const outputLog = path.resolve(path.join(dataOutput, version + '.txt'))
   fs.mkdirSync(path.dirname(jarFile), { recursive: true })
   fs.mkdirSync(path.dirname(serverDir), { recursive: true })
-  
-  async function launchServer() {
-    await new Promise((resolve, reject) => {
-      wrap['downloadServer'](version, jarFile, function (err) {
-        if (err) return reject(err)
-        resolve()
+  fs.mkdirSync(path.dirname(outputLog), { recursive: true })
+  const Item = PItem(version)
+
+  const vServer = await launchServer(version, jarFile, serverOptions)
+  const stop = () => {
+    return new Promise((resolve) => {
+      process.removeAllListeners('SIGINT')
+      vServer.stopServer(() => {
+        vServer.deleteServerData(() => {
+          console.info('Deleted server data')
+          resolve()
+        })
       })
     })
-    console.log('Download server complete !')
-    const vServer = new wrap.WrapServer(jarFile, serverDir)
-    vServer.on('line', function (line) {
-      console.log(line)
-    })
-    await new Promise((resolve, reject) => {
-      vServer.startServer(serverOptions, (err) => {
-        if (err) return reject(err)
-        resolve()
-      })
-    })
-    console.log('Server Started !')
-    return vServer
   }
-
-  async function launchBot() {
-    const bot = mf.createBot({
-      username: 'blockBot',
-      skipValidation: true,
-      version: version
-    })
-    bot.on('kicked', console.error)
-    bot.on('error', console.error)
-    bot.on('end', () => console.info('Bot disconnected'))
-
-    await once(bot, 'spawn')
-    return bot
-  }
-  const vServer = await launchServer()
-  process.once('exit', () => {
-    vServer.stopServer(() => {
-      vServer.deleteServerData(() => {console.info('Deleted server data')})
-    })
+  process.once('SIGINT', () => {
+    stop()
   })
-  const bot = await launchBot()
+  const bot = await launchBot(version)
   while (!bot.entity.onGround) {
     await wait(100)
   }
   const at = bot.entity.position.floored()
-  console.log('Building at ', at)
   const mcData = require('minecraft-data')(version)
+  bot._client.write('set_creative_slot', {
+    slot: 36,
+    item: { blockId: -1 }
+  })
+  await wait(2000)
+  console.log('Building at ', at)
+  // let [username, message] = await once(bot, 'chat')
+  // while (message !== 'start') {
+  //   [username, message] = await once(bot, 'chat')
+  // }
+  for (const [name, item] of Object.entries(mcData.itemsByName)) {
+    await bot.creative.setInventorySlot(36, new Item(item.id, 1))
 
-  for (const [name, block] of Object.entries(mcData.blocksByName)) {
-    bot.creative.setInventorySlot(36, )
+    const placeAt = bot.entity.position.floored().offset(2, 0, 0)
+    // await bot.placeBlock(bot.blockAt(placeAt.offset(0, -1, 0)), new Vec3(0, 1, 0), true)
+    
+    const blockPlace = bot._placeBlockWithOptions(bot.blockAt(placeAt.offset(0, -1, 0)), new Vec3(0, 1, 0), { swingArm: 'right', forceLook: true})
+    // console.info('Listening for', `blockUpdate:${placeAt.toString()}`)
+    const blockUpdate = onceWithTimeout(bot, `blockUpdate:${placeAt.toString()}`)
 
-    const facing = block.states.find(x => x.name === 'facing')
-    if (facing) {
-      const wall = name.includes('wall') || name === 'ladder' || name === 'tripwire_hook'
-      const itemName = wall ? name.replace('wall_', '') : name
-      // console.log('Placing', name, JSON.stringify(facing))
-      if (!mcData.itemsByName[itemName]) {
-        console.log('Cant place :', name)
-        continue
+    try {
+      const result = await Promise.all([blockPlace, blockUpdate])
+      const blockName = result[1][1].name
+      console.info('Item', name, '->', blockName)
+      fs.appendFileSync(outputLog, `${name};${blockName}\n`)
+    } catch (err) {
+      console.info('Item', name, '-> Error', err.message)
+      fs.appendFileSync(outputLog, `${name};None\n`)
+      await wait(100)
+    }
+    
+    bot.chat(`/tp ${bot.username} ~ ~ ~2`)
+    while (true) {
+      try {
+        await onceWithTimeout(bot, 'forcedMove')
+        await wait(100)
+        break
+      } catch (err) {
+        console.info('Got error teleporting', err.message)
       }
-      if (wall) {
-        await bot.builder.equipItem(mcData.itemsByName.dirt.id)
-        await bot.placeBlock(bot.blockAt(at.offset(0, -1, 4)), new Vec3(0, 1, 0))
-      }
-      await bot.builder.equipItem(mcData.itemsByName[itemName].id)
-      // try {
-
-      if (wall) await bot.placeBlock(bot.blockAt(at.offset(0, 0, 4)), new Vec3(0, 0, -1))
-      else await bot.placeBlock(bot.blockAt(at.offset(0, -1, 3)), new Vec3(0, 1, 0))
-      const result = bot.blockAt(at.offset(0, 0, 3))
-      console.log(name, result.getProperties().facing, facing.num_values)
-      await bot.dig(result)
-
-      if (wall) await bot.dig(bot.blockAt(at.offset(0, 0, 4)))
-
-      const face = result.getProperties().facing
-      if (wall) {
-        assert.ok(['north', 'south'].includes(face))
-        facingData[name] = {
-          is3D: facing.num_values > 4,
-          faceDirection: true,
-          inverted: face === 'south'
-        }
-      } else {
-        if (['up', 'down'].includes(face)) {
-          facingData[name] = {
-            is3D: facing.num_values > 4,
-            faceDirection: true,
-            inverted: face === 'down'
-          }
-        } else {
-          facingData[name] = {
-            is3D: facing.num_values > 4,
-            faceDirection: result.name.includes('trapdoor'),
-            inverted: face === 'south'
-          }
-        }
-      }
-
-      /* } catch (e){
-        console.log(name, 'cant be placed on floor', facing.num_values)
-      } */
-      await wait(50)
     }
   }
 
+  console.info('Version', version, 'done cleaning up')
+  await stop()
 }
 
-runBlocks(version)
+;(async () => {
+  for (const version of mf.supportedVersions) {
+    await runBlocks(version)
+  }
+})()
